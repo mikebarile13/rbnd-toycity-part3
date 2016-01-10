ffa59114785c84f356f23f098617426e195b856f
Mike Barile <mikebarile13@gmail.com>
2016-01-09 19:36:52 -0800

    Add 'add_to_products' method shell

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,21 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    # hmm...
+    @@products << self
   end
 end
