5ec0ce496e631c9627d69d526807306a900a0f14
Mike Barile <mikebarile13@gmail.com>
2016-01-10 17:00:01 -0800

    Added in_stock instance and class methods.

--- lib/product.rb
+++ lib/product.rb
@@ -1,21 +1,39 @@
 class Product
-  attr_reader :title
+	attr_reader :title, :price, :stock
 
-  @@products = []
+	@@products = []
 
-  def initialize(options={})
-    @title = options[:title]
-    add_to_products
-  end
+	def initialize(options={})
+    	@title = options[:title]
+    	@price = options[:price]
+    	@stock = options[:stock]
+    	add_to_products
+	end
 
-  def self.all
-    @@products
-  end
+  	#Returns true if the product is in stock i.e. stock > 0
+  	def instock?
+  		if stock > 0 
+  			return TRUE
+  		else return FALSE 
+  	end
 
-  private
+  	def self.all
+    	@@products
+  	end
 
-  def add_to_products
-    # hmm...
-    @@products << self
-  end
+  	#Returns a single product based on its title
+  	def self.find_by_title(title)
+  		#FILL THIS IN
+  	end
+
+  	#Returns an array with all in-stock products
+  	def self.in_stock
+  		#FILL THIS IN
+  	end
+
+  	private
+
+  	def add_to_products
+    	@@products << self
+  	end
 end
