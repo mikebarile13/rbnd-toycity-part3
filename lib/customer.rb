57e09af163b52a77ab97a6d19e5dc4815ac9c224
Mike Barile <mikebarile13@gmail.com>
2016-01-10 19:30:24 -0800

    No major edits. Mostly played around trying to debugg Customer.find_by_name... no success :(

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,42 +4,42 @@ class Customer
     @@customers = []
 
     def initialize(name)
-      @name = name
-      @purchases = []
-      if Customer.customer_exists?(@name)
-        raise DuplicateCustomerError, "'#{@name}' already exists"
-      else
-        add_to_customers
-      end
+        @name = name
+        @purchases = []
+        if Customer.customer_exists?(@name)
+            raise DuplicateCustomerError, "'#{@name}' already exists"
+        else
+            add_to_customers
+        end
     end
 
     # Allows a customer to "purchase" a product; generates a new transaction and adds the purchase to the customer's purchase history array
     def purchase(product)
-      transaction = Transaction.new(self, product)
-      @purchases << transaction
+        transaction = Transaction.new(self, product)
+        @purchases << transaction
     end
 
     # Returns an array with all customers
     def self.all
-      @@customers
+        @@customers
   	end
 
-  	# Returns a single product based on its title
+  	# Returns a single customer based on customer name
   	def self.find_by_name(name)
-      searched_name = @@customers.find{|customer| customer.name == name}
+        searched_name = @@customers.find{|customer| customer.name == name}
     end
 
     # Checks to see if a customer is already in the database
     def self.customer_exists?(name)
-      exists = false
-      @@customers.each{|customer| if customer.name == name then exists = true end}
-      exists
+        exists = false
+        @@customers.each{|customer| if customer.name == name then exists = true end}
+        exists
     end
 
   	private
 
     # Adds to an array with a list of all customers
   	def add_to_customers
-    	@@customers << self
+    	  @@customers << self
   	end
 end
